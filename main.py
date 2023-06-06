06611f3a87cadf19cd9df4994a382883e4bbdefb
Yevhen Kaskov <kaskov.e@gmail.com>
2023-06-06 14:36:28 +0300

    test connection

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import uvicorn
+# import uvicorn
+#
+# if __name__ == "__main__":
+#     uvicorn.run("server.app:app", host="127.0.0.1", port=8000, reload=True)
 
-if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="127.0.0.1", port=8000, reload=True)
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
