2c98faef6a35c35b23109f2aff2e2486eee67bc7
Yevhen Kaskov <kaskov.e@gmail.com>
2023-06-06 21:28:47 +0300

    connect to mongoDB added

--- main.py
+++ main.py
@@ -3,9 +3,12 @@
 # if __name__ == "__main__":
 #     uvicorn.run("server.app:app", host="127.0.0.1", port=8000, reload=True)
 
-
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import motor.motor_asyncio
+
 
 app = FastAPI()
 
@@ -33,3 +36,27 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+load_dotenv()
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
+# подключаемся к БД catalog, если её нет, то будет создана
+db = client.catalog
+
+collection = db["wines"]
+
+wine1 = {
+    "name": "Кампаньола Піно Гріджіо Венеціє",
+    "description": "some description",
+    "color": "White",
+    "type": "Dry",
+    "brand": "Campagnola",
+    "country": "Italy",
+    "region": "Veneto",
+    "grape_variety": ["аперитив", "морепродукти", "салати"],
+    "capacity": 0.75,
+    "price": 3257.00,
+    "alcohol_percentage": 10.6,
+    "classification": "IGT",
+}
+
+ins_result = collection.insert_one(wine1)  # добавляет одну запись в коллекцию collection
