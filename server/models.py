3d9281a2d5a29e7c3fccfb3c970eddb971bfe5b6
Nikyxa <kolisova.veronika81@gmail.com>
2023-06-19 19:25:38 +0300

    Add 2 User model and Comment model

--- server/models.py
+++ server/models.py
@@ -1,5 +1,21 @@
-from bson import ObjectId
+from typing import Optional, List
+
 from pydantic import BaseModel
+
+
+class User(BaseModel):
+    id: str
+    name: str
+    email: str
+    password: str
+    telephone: Optional[str] = None
+
+
+class Comment(BaseModel):
+    id: int
+    text: str
+    wine_id: str
+    users: List[User]
 
 
 class Wine(BaseModel):
@@ -22,3 +38,4 @@ class Wine(BaseModel):
     price: str
     image_url: str
     small_image_url: str
+    comment_id: int
